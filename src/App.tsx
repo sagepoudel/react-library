c15c2ddcb841de7a0d2e12b6a1eff25321e7c66a
sagepoudel <pdlsaroj22@gmail.com>
2023-01-03 16:37:08 -0800

    add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/navbarAndFooter/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the full stack</h1>
-    </div>
+<Navbar/>
   );
 }
 
